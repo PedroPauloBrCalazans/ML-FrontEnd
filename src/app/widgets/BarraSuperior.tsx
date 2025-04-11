78f7acbd430966ff6fc68ab962243768e298c799
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-10 23:07:25 -0300

    Finalização da barra superior + arquivos excluidos.

--- src/app/widgets/BarraSuperior.tsx
+++ src/app/widgets/BarraSuperior.tsx
@@ -1,7 +1,30 @@
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function BarraSuperior() {
   return (
     <div className="">
-      <header className="bg-amber-300">Barra</header>
+      <div className="container mx-auto w-full px-4 py-2 flex items-center justify-between">
+        <Link href="/">
+          <Image src="/logo.png" alt="Mercado Livre" width={180} height={180} />
+        </Link>
+
+        <div className="border border-gray-200 bg-white flex items-center flex-1 max-w-xl mx-4">
+          <input
+            type="text"
+            placeholder="Buscar produtos"
+            className="w-full px-4 py-2"
+          />
+          <button className="text-gray-200 px-4 py-2">
+            <Search size={24} />
+          </button>
+        </div>
+
+        <button className="bg-purple-600 text-white px-4 py-2 rounded-full text-sm hover:bg-purple-700">
+          Assine
+        </button>
+      </div>
     </div>
   );
 }
