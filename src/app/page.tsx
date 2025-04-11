86e3893f46e90cbbd5ade9001c4bddf6ce463b3c
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-10 22:36:26 -0300

    Estrutura base do projeto + Componente da barra superior

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import BarraSuperior from "./widgets/BarraSuperior";
+
+export default function Home() {
+  return (
+    <div>
+      <header className="bg-amber-300">
+        <BarraSuperior />
+      </header>
+    </div>
+  );
+}
---
+++ src/app/widgets/BarraSuperior.tsx
@@ -0,0 +1,7 @@
+export default function BarraSuperior() {
+  return (
+    <div className="">
+      <header className="bg-amber-300">Barra</header>
+    </div>
+  );
+}
