03877325455d91f1022bd57fc242b9110c20bd22
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-10 23:24:08 -0300

    Implementação do componente de menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BarraSuperior from "./widgets/BarraSuperior";
+import MenuNavegacao from "./widgets/MenuNavegacao";
 
 export default function Home() {
   return (
     <div>
       <header className="bg-amber-300">
         <BarraSuperior />
+        <MenuNavegacao />
       </header>
     </div>
   );
